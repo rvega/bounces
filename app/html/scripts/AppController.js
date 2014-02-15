66a31ceb8fc1aac1cb2eca0784574b94e2f584b7
Rafael Vega <email.rafa@gmail.com>
2014-02-15 18:18:20 -0500

    started implementation of GUI

--- app/html/scripts/AppController.js
+++ app/html/scripts/AppController.js
@@ -1,13 +1,13 @@
-define(['./Listener', './Source'], function(Listener, Source){
+define(['./Listener', './Source', 'vendors/oss/knob/knob', 'vendors/oss/slider/slider'], function(Listener, Source, Knob, Slider){
    var AppController = function(){
-      this.canvasWidth = 250;
-      this.canvasHeight = 250;
+      this.canvasWidth = 600;
+      this.canvasHeight = 600;
 
       this.initStages();
       this.initListener();
-      this.initPD(function(){
+      // this.initPD(function(){
          this.initSources();
-      }.bind(this));
+      // }.bind(this));
    };
 
    AppController.prototype.initPD = function(callback){
@@ -71,6 +71,29 @@ define(['./Listener', './Source'], function(Listener, Source){
    };
 
    AppController.prototype.initStages = function(){
+      $('#seek').slider({
+         step: 0.000001,
+         min: 0,
+         max: 1,
+         value: 0,
+         tooltip: 'hide'
+      });
+
+      var volume = new Knob({
+         domElementId: 'volume',
+         changingCallback: function(value, normalizedValue){
+            console.log('vol');
+         },
+         value: 0.6,
+         pow: 1,
+         minValue: 0,
+         maxValue: 1,
+         width: '30px',
+         height: '30px',
+         speed: 10,
+         spriteInitialAngle: 270,
+      });
+      
       $('#canvas-stage').attr('width', this.canvasWidth).attr('height', this.canvasHeight);
       this.stage = new fabric.Canvas('canvas-stage',{
          backgroundColor: '#AAA'
